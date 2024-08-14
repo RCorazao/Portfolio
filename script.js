156ed1d1a46f3d7b6feb00e6918d32bb0d29b843
ReisonC <128325868+RCorazao@users.noreply.github.com>
2024-08-13 21:10:20 -0500

    Update

--- script.js
+++ script.js
@@ -52,3 +52,37 @@ document.addEventListener('DOMContentLoaded', () => {
       }
   });
 });
+
+// Contact section
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', handleSubmit);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(this);
+
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: formData,
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    this.reset();
+    showAlert();
+  }
+}
+
+function showAlert() {
+  document.getElementById('customAlert').style.display = 'block';
+  setTimeout( () => {
+    closeAlert();
+  }, 3500 );
+}
+
+function closeAlert() {
+  document.getElementById('customAlert').style.display = 'none';
+}
